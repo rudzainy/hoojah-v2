7d1042a12bc81e92808ad47588a91fb088c5cfaf
rudzainy <rudzainy@gmail.com>
2016-08-15 19:32:24 +0800

    Create stores

---
+++ frontend/stores/opinion-store.react.js
@@ -0,0 +1,8 @@
+import DefaultStore from './default-store.react';
+import assign from 'object-assign';
+
+const OpinionStore = assign({}, DefaultStore.prototype, {
+
+});
+
+export default OpinionStore;
